98f350c5729a4f1bc6d66296613a490d364ab116
Chaodan Luo <amyluo0421@gmail.com>
2021-05-20 16:12:24 -0700

    Edited bci_music.py

--- bci_music.py
+++ bci_music.py
@@ -1,34 +1,50 @@
 from pygame import mixer
+import time
   
 # Starting the mixer
 mixer.init()
   
 # Loading the song
-mixer.music.load("Because I'm Happy - Happy.mp3")
+mixer.music.load("happy (because I'm happy).mp3")
   
 # Setting the volume
-mixer.music.set_volume(0.7)
+mixer.music.set_volume(0.2)
   
 # Start playing the song
 mixer.music.play()
-  
-# infinite loop
+
 while True:
+    time.sleep(5)
+    mixer.music.pause()
+    time.sleep(5)
+    mixer.music.unpause()
+
+# dif conditions 
+if "happy":
+    mixer.music.load("happy (because I'm happy).mp3")
+  
+if "sad":
+    mixer.music.load("sad (say something).mp3")
+
+
+# infinite loop
+# while True:
       
-    print("Press 'p' to pause, 'r' to resume")
-    print("Press 'e' to exit the program")
-    query = input("  ")
+#     print("Press 'p' to pause, 'r' to resume")
+#     print("Press 'e' to exit the program")
+#     query = input("  ")
       
-    if query == 'p':
+#     if query == 'p':
   
-        # Pausing the music
-        mixer.music.pause()     
-    elif query == 'r':
+#         # Pausing the music
+#         mixer.music.pause()     
+#     elif query == 'r':
   
-        # Resuming the music
-        mixer.music.unpause()
-    elif query == 'e':
+#         # Resuming the music
+#         mixer.music.unpause()
+#     elif query == 'e':
   
-        # Stop the mixer
-        mixer.music.stop()
-        break
+#         # Stop the mixer
+#         mixer.music.stop()
+#         break
+
